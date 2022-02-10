610f9dcdd988b0e100aabb5e96f2d12b60353003
igor-casatti <igor-casatti@users.noreply.github.com>
2022-02-10 22:36:47 +0100

    Spectral Connectivity

--- epoch_connectivity/ConnEEG.py
+++ epoch_connectivity/ConnEEG.py
@@ -5,7 +5,8 @@ import mne_connectivity as mnecon
 from epoch_connectivity.conn_epoch import EpochConnection
 from epoch_connectivity.utils import only_EEG_channels
 import scipy
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 """
 
@@ -14,7 +15,7 @@ of transforming EEGs into connectivity matrices
 
 """
 
-class EEGConnection(object):
+class EEG_DirectedConnection(object):
 
     def __init__(self, epoched_eeg):
         #self.epoched_eeg = epoched_eeg
@@ -27,8 +28,8 @@ class EEGConnection(object):
 
     def fit_MVAR_on_epochs(self, model_order=20):
         """
-        Fit on MVAR on each epoch. The MVAR order is calculated for each epoch. The ScoT library fits
-        the model with defined order and optimize the regularization.
+        Fit on MVAR on each epoch. The ScoT library fits the model with defined
+        order and optimize the regularization.
 
         -----------
         PARAMETERS
@@ -42,7 +43,11 @@ class EEGConnection(object):
         
         """
         varx = scot.var.VAR(model_order=model_order)
-        self.result = scot.varica.mvarica(x=self.epoch_matrix, var=varx, reducedim='no_pca', optimize_var=True, varfit='trial')
+        result = scot.varica.mvarica(x=self.epoch_matrix, var=varx, reducedim='no_pca', optimize_var=True, varfit='trial')
+
+        # By getting result.a.coef we get the coefficients on EEG space, result.b.coef are the coefs in source space (ICA)
+        self.coefs = result.a.coef
+        self.rescov = result.a.rescov
 
     
     def integrated_measure_epochs(self, measure_name, frequency_band = None, nfft = None):
@@ -67,7 +72,7 @@ class EEGConnection(object):
         if nfft is None:
             nfft = 2 * self.sfreq
 
-        self.conn = scot.Connectivity(b = self.result.a.coef, c = self.result.a.rescov, nfft=nfft)
+        self.conn = scot.Connectivity(b = self.coefs, c = self.rescov, nfft=nfft)
 
         if measure_name == 'dDTF':
             measure = self.conn.dDTF()
@@ -88,4 +93,45 @@ class EEGConnection(object):
         integrated_measure = scipy.integrate.simps(y=measure[:,:,i:j+1], x=x[i:j+1])
         
         return integrated_measure
+        
+#TODO https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
+
+#TODO save function
+# pickle.dump(myConn, open( "save.p", "wb" ))
+# myConnLoaded = pickle.load( open( "save.p", "rb" ) )
+
+#TODO another plot (?) <- for later
+
+#TODO wPLI https://mne.tools/mne-connectivity/stable/auto_examples/cwt_sensor_connectivity.html
+
+
+class EEG_SpectralConnection(object):
+    
+    def __init__(self, epoched_eeg):
+        self.epoched_eeg = epoched_eeg
+        #self.ch_names = only_EEG_channels(epoched_eeg.ch_names)
+        #self.sfreq = epoched_eeg.info['sfreq']
+        #self.epoch_matrix = epoched_eeg.get_data(picks=['eeg'])
+        #self.n_epochs = len(self.epoch_matrix)
+    
+    def spec_connectivity(self, measure = 'wpli', frequency_band = None):
+        if frequency_band is None:
+            f_min, f_max = 0, np.inf
+        else:
+            f_min, f_max = frequency_band[0], frequency_band[1]
+        
+        con = mnecon.spectral_connectivity_epochs(data=self.epoched_eeg, method=measure, mode='multitaper',
+                                              fmin=f_min, fmax=f_max, faverage=True, verbose=False)
+        
+        con_matrix = con.get_data(output='dense')[:,:,0]
+
+        # Get only channels that are EEG:
+        channel_indices = mne.pick_types(self.epoched_eeg.info, eeg=True) # Indices of EEG channels
+        # Return the connective matrix just with the EEG channels connectivity
+        return con_matrix[np.ix_(channel_indices, channel_indices)]
+    
+    def spec_connectivity(self, measure = 'wpli', frequency_band = None):
+        con_matrix = self.spec_connectivity(measure=measure, frequency_band=frequency_band)
+        ax = sns.heatmap(con_matrix, linewidth=0.1)
+        plt.show()
         
