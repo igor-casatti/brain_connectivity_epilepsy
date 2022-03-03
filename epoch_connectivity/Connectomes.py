0255049dbd579f8fb55dd4d1d2eb46355491929e
igor-casatti <igor-casatti@users.noreply.github.com>
2022-03-03 03:03:26 +0100

    Connectome

---
+++ epoch_connectivity/Connectomes.py
@@ -0,0 +1,6 @@
+import epoch_connectivity.ConnEEG as cnegg
+from epoch_connectivity.utils import FrequencyBand, only_EEG_channels
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import networkx as nx
